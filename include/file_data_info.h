8e701514af571a1837f05d584cf18254a805317e
stelro <stelmach.ro@gmail.com>
2016-01-18 03:16:20 +0200

    implement of the file_data_info

---
+++ include/file_data_info.h
@@ -0,0 +1,44 @@
+#ifndef GUARD_FILE_DATA_INFO
+#define GUARD_FILE_DATA_INFO
+
+#include "extensions_lib.h"
+
+namespace ste {
+
+    /* this class , track information about every unique file */
+
+    class FileInfo : public ExtensionHelper {
+    private:
+        size_t chars_counter;
+        size_t lines_counter;
+        size_t blank_counter;
+        size_t single_comments_counter;
+        size_t multi_comments_counter;
+        size_t file_number;
+        bool unique;
+        bool empty;
+        std::string file_extension;
+    public:
+        FileInfo();
+        FileInfo(const FileInfo &copy_file);
+        void set_chars(const size_t chars) { chars_counter = chars; }
+        void set_lines(const size_t lines) { lines_counter = lines; }
+        void set_file_number(const size_t file_no) { file_number = file_no; }
+        void set_extension(const std::string &ext) { file_extension = ext; }
+        void set_blanks(const size_t blank) { blank_counter = blank; }
+        void set_s_comments(const size_t s_comments) { single_comments_counter = s_comments; }
+        void set_m_comments(const size_t m_comments) { multi_comments_counter = m_comments; }
+        size_t get_chars() const { return chars_counter; }
+        size_t get_lines() const { return lines_counter; }
+        size_t get_blnk_lines() const { return blank_counter; }
+        size_t get_s_comments() const { return single_comments_counter; }
+        size_t get_m_comments() const { return multi_comments_counter; }
+        size_t get_file_number() const { return file_number; }
+        bool is_unique() const { return unique; }
+        bool is_empty() const { return empty; }
+        std::string get_file_extension() const { return file_extension; }
+
+    };
+}
+
+#endif
---
+++ src/file_data_info.cpp
@@ -0,0 +1,1 @@
+#include "../include/file_data_info.h"
