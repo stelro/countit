3e083071cb13ee62765862639a3ca17ea131e843
stelro <stelmach.ro@gmail.com>
2016-01-27 00:19:03 +0200

    open_directories function

--- include/opendir.h
+++ include/opendir.h
@@ -3,12 +3,15 @@
 
 #include <iostream>
 #include <vector>
+#include <dirent.h>
+#include <cstring>
 
-class DirTools {
-public:
-    std::vector<std::string> open_directories(const std::string path_dir);
-    static void parseDirectory(const std::string &reading_directory, const int level, std::vector<std::string> &tmp);
-};
-
+namespace stel {
+    class DirTools {
+    public:
+        std::vector<std::string> open_directories(const std::string &path_dir);
+        static void parseDirectory(const std::string &reading_directory, const int level, std::vector<std::string> &tmp);
+    };
+}
 
 #endif
--- src/opendir.cpp
+++ src/opendir.cpp
@@ -1,1 +1,31 @@
 #include "../include/opendir.h"
+
+namespace stel {
+
+    //FIXME: return reference vector
+    std::vector<std::string> DirTools::open_directories(const std::string &path_dir)
+    {
+        DIR *directory;
+        dirent *pdir;
+        std::vector<std::string> files_names;
+        directory = opendir(path_dir.c_str());
+        std::string special("");
+
+        while ((pdir = readdir(directory))) {
+            //--------------------
+            //ignore folders
+            //--------------------
+
+            if (!strcmp(pdir->d_name, ".") || !strcmp(pdir->d_name, ".."))
+                continue;
+            // return's the full path
+            special = path_dir + '/' + pdir->d_name;
+            files_names.push_back(special);
+
+        }
+
+        closedir(directory);
+
+        return files_names;
+    }
+}
