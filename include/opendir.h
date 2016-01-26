95e8c6dd79c0ec23eec35c7c2c370abbeb87eb83
stelro <stelmach.ro@gmail.com>
2016-01-27 00:10:52 +0200

    parse directories header

--- include/opendir.h
+++ include/opendir.h
@@ -1,4 +1,14 @@
 #ifndef GUARD_OPENDIR_H
 #define GUARD_OPENDIR_H
 
+#include <iostream>
+#include <vector>
+
+class DirTools {
+public:
+    std::vector<std::string> open_directories(const std::string path_dir);
+    static void parseDirectory(const std::string &reading_directory, const int level, std::vector<std::string> &tmp);
+};
+
+
 #endif
